16af37805615b802e42e885c30b6eca2972996ac
LaMelD <wqe014@gmail.com>
2020-03-05 20:10:55 +0900

    init commit

---
+++ InteriorHelper/Assets/2_Script/ButtonCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCtrl : MonoBehaviour
+{
+    public Transform Prefabs;
+    public GameObject floor;
+
+    public void newObject()
+    {
+        Instantiate(Prefabs, new Vector3(0, 0, 0), Quaternion.identity).transform.SetParent(floor.transform);
+    }
+}
---
+++ InteriorHelper/Assets/2_Script/TitleCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+
+public class TitleCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // StartBtn click function
+    public void LoginBtn()
+    {
+        //to do :: when Login button clicked
+    }
+
+    public void GuestBtn()
+    {
+        SceneManager.LoadScene("2_2DScene");
+    }
+
+    public void LoadBtn()
+    {
+        //to do :: when LOAD_LOCAL button clicked
+    }
+}
