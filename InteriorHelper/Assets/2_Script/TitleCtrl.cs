e89d00d32cb9d51941086fd57d4b14e277796096
LaMelD <wqe014@gmail.com>
2020-03-05 22:23:11 +0900

    update Title Scene -- button control added

--- InteriorHelper/Assets/2_Script/TitleCtrl.cs
+++ InteriorHelper/Assets/2_Script/TitleCtrl.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
-
+using UnityEngine.UI;
 
 public class TitleCtrl : MonoBehaviour
 {
+    public GameObject firstpanel;
+    public GameObject floorpanel;
+
+    public InputField garo;
+    public InputField sero;
+    public InputField nophi;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstpanel.SetActive(true);
+        floorpanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,15 +31,35 @@ public class TitleCtrl : MonoBehaviour
     public void LoginBtn()
     {
         //to do :: when Login button clicked
+        firstpanel.SetActive(false);
+        floorpanel.SetActive(true);
     }
 
     public void GuestBtn()
     {
-        SceneManager.LoadScene("2_2DScene");
+        firstpanel.SetActive(false);
+        floorpanel.SetActive(true);
+        //SceneManager.LoadScene("2_2DScene");
     }
 
     public void LoadBtn()
     {
         //to do :: when LOAD_LOCAL button clicked
+    }
+
+    public void NextButton()
+    {
+        Debug.Log(garo.text);
+        Debug.Log(sero.text);
+        Debug.Log(nophi.text);
+    }
+
+    public void CancelButton()
+    {
+        garo.text = "";
+        sero.text = "";
+        nophi.text = "";
+        firstpanel.SetActive(true);
+        floorpanel.SetActive(false);
     }
 }
