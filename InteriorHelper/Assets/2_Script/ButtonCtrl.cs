9d779444499653ce7bd71ff7f75dd1fa7b5dd267
Meantint <meantint@gmail.com>
2020-03-07 15:28:40 +0900

    InputFieldPanel update

--- InteriorHelper/Assets/2_Script/ButtonCtrl.cs
+++ InteriorHelper/Assets/2_Script/ButtonCtrl.cs
@@ -5,11 +5,37 @@ using UnityEngine.UI;
 
 public class ButtonCtrl : MonoBehaviour
 {
-    public Transform Prefabs;
-    public GameObject floor;
+    private bool state = false;
 
+   
+    public GameObject floor;
+    public GameObject InputFieldPanel;
+
+    public InputField inputGaro;
+    public InputField inputSero;
+    public InputField inputNophi;
+
+    public Transform RootPrefab;
+
+    void Start()
+    {
+        InputFieldPanel.SetActive(false);
+    }
+    public void NewButtonClicked()
+    {
+        InputFieldPanel.SetActive(true);
+    }
     public void newObject()
     {
-        Instantiate(Prefabs, new Vector3(0, 0, 0), Quaternion.identity).transform.SetParent(floor.transform);
+        RootPrefab.localScale = new Vector3(int.Parse(inputGaro.text), int.Parse(inputSero.text), int.Parse(inputNophi.text));
+        Instantiate(RootPrefab, new Vector3(0, 0, 0), Quaternion.identity).transform.SetParent(floor.transform);
+    }   
+    public void NextButtonClicked()
+    {
+        newObject();
+        inputGaro.text = "";
+        inputSero.text = "";
+        inputNophi.text = "";
+        InputFieldPanel.SetActive(false);
     }
 }
