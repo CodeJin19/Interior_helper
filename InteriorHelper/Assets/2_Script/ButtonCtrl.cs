0c23b77178327664680e27d2314a0d531f72500c
LaMelD <wqe014@gmail.com>
2020-03-09 03:54:13 +0900

    restart add .gitignore

---
+++ InteriorHelper/Assets/2_Script/ButtonCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCtrl : MonoBehaviour
+{
+    private bool state = false;
+
+   
+    public GameObject floor;
+    public GameObject InputFieldPanel;
+
+    public InputField inputGaro;
+    public InputField inputSero;
+    public InputField inputNophi;
+
+    public Transform RootPrefab;
+
+    void Start()
+    {
+        InputFieldPanel.SetActive(false);
+    }
+    public void NewButtonClicked()
+    {
+        InputFieldPanel.SetActive(true);
+    }
+    public void newObject()
+    {
+        RootPrefab.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector3(int.Parse(inputGaro.text), int.Parse(inputSero.text), int.Parse(inputNophi.text));
+        Instantiate(RootPrefab, new Vector3(0, 0, 0), Quaternion.identity).transform.SetParent(floor.transform);
+    }   
+    public void NextButtonClicked()
+    {
+        newObject();
+        inputGaro.text = "";
+        inputSero.text = "";
+        inputNophi.text = "";
+        InputFieldPanel.SetActive(false);
+    }
+}
---
+++ InteriorHelper/Assets/2_Script/FloorCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCtrl : MonoBehaviour
+{
+    public GameObject floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floor.GetComponent<RectTransform>().sizeDelta = new Vector3(TitleCtrl.garoto2D, TitleCtrl.seroto2D, TitleCtrl.nophito2D);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
