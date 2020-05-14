ed090737bc5eb81e104a2ee1720e702d34d066c6
LaMelD <wqe014@gmail.com>
2020-05-14 19:20:14 +0900

    json test code & spring

---
+++ InteriorHelper/Assets/2_Script/test_buttonCtrl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+using System.Text;
+using System;
+
+public class test_buttonCtrl : MonoBehaviour
+{
+    public string LoginUrl;
+    public string RegisterUrl;
+
+    public InputField regid;
+    public InputField logid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginUrl = "http://54.180.142.181/login";
+        RegisterUrl = "http://54.180.142.181/register";
+    }
+
+    // Update is called once per frame
+    public void LoginBtn()
+    {
+        StartCoroutine(LoginCo());
+    }
+
+    IEnumerator LoginCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", logid.text);
+
+        WWW webRequest = new WWW(LoginUrl, form);
+        yield return webRequest;
+
+        /*
+        // utf-8 인코딩
+        byte[] bytesForEncoding = Encoding.UTF8.GetBytes(webRequest.text);
+        string encodedString = Convert.ToBase64String(bytesForEncoding);
+
+        // utf-8 디코딩
+        byte[] decodedBytes = Convert.FromBase64String(encodedString);
+        string decodedString = Encoding.UTF8.GetString(decodedBytes);
+        
+        Debug.Log(decodedString);
+        */
+
+        Debug.Log(webRequest.text);
+    }
+
+    public void RegisterBtn()
+    {
+        StartCoroutine(RegisterCo());
+    }
+
+    IEnumerator RegisterCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", regid.text);
+
+        WWW webRequest = new WWW(RegisterUrl, form);
+        yield return webRequest;
+
+        /*
+        // utf-8 인코딩
+        byte[] bytesForEncoding = Encoding.UTF8.GetBytes(webRequest.text);
+        string encodedString = Convert.ToBase64String(bytesForEncoding);
+
+        // utf-8 디코딩
+        byte[] decodedBytes = Convert.FromBase64String(encodedString);
+        string decodedString = Encoding.UTF8.GetString(decodedBytes);
+
+        Debug.Log(decodedString);
+        */
+
+        Debug.Log(webRequest.text);
+    }
+}
---
+++ InteriorHelper/Assets/2_Script/test_jsonCtrl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using LitJson;
+using System.IO;
+
+public class Data
+{
+    public int id;
+    public string floor;
+    public List<string> objects;
+
+    public Data(int id, string floor, List<string> objects)
+    {
+        this.id = id;
+        this.floor = floor;
+        this.objects = objects;
+    }
+
+    public void Addobject(string obj)
+    {
+        this.objects.Add(obj);
+    }
+}
+
+public class test_jsonCtrl : MonoBehaviour
+{
+    public Data test = new Data(1, "floor", new List<string>());
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test.Addobject("closet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadBtn()
+    {
+
+    }
+
+    public void SaveBtn()
+    {
+        Debug.Log("저장하기");
+
+        JsonData ItemJson = JsonMapper.ToJson(test);
+        File.WriteAllText(Application.dataPath + "/Resources/data/test.json", ItemJson.ToString());
+
+        Debug.Log(ItemJson.ToString());
+        Debug.Log(Application.dataPath + "/test.json");
+    }
+}
